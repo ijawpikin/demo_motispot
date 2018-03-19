b01ece9fd4b94d47db0fb688b246d3317a11c77e
ayibatonye ikemike <ijawpikin@gmail.com>
2018-03-19 02:39:36 +0100

    initial of demo_notispot

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
+  get 'products/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
